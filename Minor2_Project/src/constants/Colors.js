fe1b16064e7353c8bed139edc9f628622780df8b
Biditmangal <biditmangal9506@gmail.com>
2021-03-13 23:39:04 +0530

    Added multiple files

---
+++ Minor2_Project/src/constants/Colors.js
@@ -0,0 +1,19 @@
+const tintColor = '#2f95dc';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+
+  // Theme Styling
+  // themeBackground: '#D0C2B9',
+  // headerColor: '#E5B8B7',
+
+};
---
+++ Minor2_Project/src/constants/Theme.js
@@ -0,0 +1,17 @@
+const MyTheme = {
+  dark: false,
+  colors: {
+    primary: '#000000',
+    background: '#295D75',
+    card: '#295D75',
+    buttonColor: '#5A77DF',
+    buttonText: '#fff',
+    text: '#000000',
+    headerColor: "#295D75",
+    heading: '#000000',
+    backgroundWhite: '#ffffff',
+    formOuter: '#E8F5FF'
+  },
+};
+
+export default MyTheme;
