5347922962ef22c2ede517132902c0f5a1098965
Biditmangal <biditmangal9506@gmail.com>
2021-03-20 21:27:01 +0530

    Created Article Card

---
+++ Minor2_Project/src/components/ArticleCard.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {StyleSheet, View, Image, Text} from 'react-native';
+import {Card} from 'react-native-elements';
+
+const ArticleCard = (props) => {
+  return (
+    <>
+      <Card containerStyle={styles.container}>
+        <View
+          style={{
+            flexDirection: 'row',
+            marginVertical: 10,
+          }}>
+          <View
+            style={{
+              flex: 5,
+              marginLeft: 10,
+            }}>
+            <Text style={styles.title}>{props.title}</Text>
+            <Text style={styles.description}>{props.description}</Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              marginRight: 50,
+              justifyContent: 'flex-end',
+            }}>
+            <Image
+              style={styles.image}
+              resizeMode={'cover'}
+              source={{uri: props.thumbnail}}
+            />
+          </View>
+        </View>
+      </Card>
+    </>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 15,
+    margin: 10,
+    padding: 10,
+  },
+  image: {
+    height: 100,
+    width: 100,
+    borderRadius: 15,
+  },
+  description: {
+    fontSize: 14,
+    color: '#172D45',
+    marginTop: 10,
+    marginRight: 10,
+  },
+  title: {
+    fontSize: 24,
+    color: '#172D45',
+    marginTop: 5,
+  },
+  articleDesp: {
+    fontSize: 14,
+    marginBottom: 5,
+  },
+});
+
+export default ArticleCard;
