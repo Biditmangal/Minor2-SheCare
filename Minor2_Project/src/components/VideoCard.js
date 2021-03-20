d742585648a5c1c997864bd269d545249cac1ff8
Biditmangal <biditmangal9506@gmail.com>
2021-03-20 21:27:12 +0530

    Created Video Card

---
+++ Minor2_Project/src/components/VideoCard.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Card} from 'react-native-paper';
+import {StyleSheet} from 'react-native';
+
+const borderRadius=15;
+const VideoCard = (props) => {
+  return (
+    <>
+      <Card style={styles.postCardStyle}>
+        <Card.Cover
+          source={{uri: props.thumbnail}}
+          style={{
+            borderTopRightRadius: borderRadius,
+            borderTopLeftRadius: borderRadius,
+          }}
+        />
+        <Card.Title
+          title={props.videoTitle}
+          subtitle={props.videoAuthor}
+          titleStyle={{
+            color: '#263238',
+            fontSize: 20,
+          }}
+          subtitleStyle={{
+            fontSize: 14,
+            color: '#b5b5b5',
+          }}
+        />
+      </Card>
+    </>
+  );
+};
+const styles = StyleSheet.create({
+  postCardStyle: {
+    width: 170,
+    height: 270,
+    margin: 5,
+    borderRadius: borderRadius,
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+  },
+});
+export default VideoCard;
