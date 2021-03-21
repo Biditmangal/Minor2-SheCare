e2d2e76f7012c3e8ee526666e3f36a8bccf6edc3
Biditmangal <biditmangal9506@gmail.com>
2021-03-22 02:32:34 +0530

    Updated some packages

--- Minor2_Project/App.js
+++ Minor2_Project/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import Information from './src/screens/Main/Information.js';
+import Profile from './src/screens/Main/Profile.js';
 
 const App = () => {
-  
+  const item = {
+    prImage: 'https://picsum.photos/720',
+    prName: 'Giana Dias',
+    prUsername: 'gianadias',
+    prDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est ut aenean leo nibh leo adipiscing. Odio id in ac augue vitae. Dolor vulputate libero est ut. Scelerisque sed cursus tristique proin ipsum pellentesque. Ut et quam ultricies.',
+  };
   return (
     <>
       <View style={styles.container}>
-        <Information/>
+        {/* <Information/> */}
+        <Profile item={item} />
       </View>
     </>
   );
