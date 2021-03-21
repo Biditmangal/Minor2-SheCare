60b35494c05f35eeea58fb185eed81a2ab2babf9
Biditmangal <biditmangal9506@gmail.com>
2021-03-21 23:41:20 +0530

    Completed the Information Page

--- Minor2_Project/src/screens/Main/Information.js
+++ Minor2_Project/src/screens/Main/Information.js
@@ -1,24 +1,130 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {FlatList, StyleSheet, View, ScrollView} from 'react-native';
 import ArticleCard from '../../components/ArticleCard';
 import VideoCard from '../../components/VideoCard';
 
 const Information = () => {
-    const videoItem = {
-      thumbnail: 'https://picsum.photos/700',
-      videoTitle: 'Health Issues',
-      videoAuthor: 'Nutritionist',
-    };
-    const articleItem = {
-      thumbnail: 'https://picsum.photos/700',
-      title: 'Health Cure',
-      description: 'LoremIpsum and bla bla',
-    };
+  const [data, setData] = useState({
+    VideoData: [
+      {
+        videoId: 1,
+        thumbnail: 'https://picsum.photos/700',
+        videoTitle: 'Health Issues',
+        videoAuthor: 'Nutritionist',
+      },
+      {
+        videoId: 2,
+        thumbnail: 'https://picsum.photos/700',
+        videoTitle: 'Health Issues',
+        videoAuthor: 'Nutritionist',
+      },
+      {
+        videoId: 3,
+        thumbnail: 'https://picsum.photos/700',
+        videoTitle: 'Health Issues',
+        videoAuthor: 'Nutritionist',
+      },
+      {
+        videoId: 4,
+        thumbnail: 'https://picsum.photos/700',
+        videoTitle: 'Health Issues',
+        videoAuthor: 'Nutritionist',
+      },
+      {
+        videoId: 5,
+        thumbnail: 'https://picsum.photos/700',
+        videoTitle: 'Health Issues',
+        videoAuthor: 'Nutritionist',
+      },
+      {
+        videoId: 6,
+        thumbnail: 'https://picsum.photos/700',
+        videoTitle: 'Health Issues',
+        videoAuthor: 'Nutritionist',
+      },
+      {
+        videoId: 7,
+        thumbnail: 'https://picsum.photos/700',
+        videoTitle: 'Health Issues',
+        videoAuthor: 'Nutritionist',
+      },
+    ],
+    ArticleData: [
+      {
+        articleId: 1,
+        thumbnail: 'https://picsum.photos/700',
+        title: 'Health Cure',
+        description: 'LoremIpsum and bla bla',
+      },
+      {
+        articleId: 2,
+        thumbnail: 'https://picsum.photos/700',
+        title: 'Health Cure',
+        description: 'LoremIpsum and bla bla',
+      },
+      {
+        articleId: 3,
+        thumbnail: 'https://picsum.photos/700',
+        title: 'Health Cure',
+        description: 'LoremIpsum and bla bla',
+      },
+      {
+        articleId: 4,
+        thumbnail: 'https://picsum.photos/700',
+        title: 'Health Cure',
+        description: 'LoremIpsum and bla bla',
+      },
+      {
+        articleId: 5,
+        thumbnail: 'https://picsum.photos/700',
+        title: 'Health Cure',
+        description: 'LoremIpsum and bla bla',
+      },
+      {
+        articleId: 6,
+        thumbnail: 'https://picsum.photos/700',
+        title: 'Health Cure',
+        description: 'LoremIpsum and bla bla',
+      },
+      {
+        articleId: 7,
+        thumbnail: 'https://picsum.photos/700',
+        title: 'Health Cure',
+        description: 'LoremIpsum and bla bla',
+      },
+    ],
+  });
+
   return (
     <>
       <View style={styles.container}>
-          {VideoCard(videoItem)}
-          {ArticleCard(articleItem)}
+        <View
+          style={{
+            flexDirection: 'row',
+            marginBottom: 5,
+            // borderWidth: 1,
+            // borderColor: '#FF0000',
+          }}>
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+            horizontal={true}
+            keyExtractor={(item) => `${item.videoId}`}
+            data={[...data.VideoData]}
+            renderItem={({item}) => <VideoCard item={item} />}
+            style={{
+              flex: 2,
+            }}
+          />
+        </View>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+          horizontal={false}
+          keyExtractor={(item) => `${item.articleId}`}
+          data={[...data.ArticleData]}
+          renderItem={({item}) => <ArticleCard item={item} />}
+        />
       </View>
     </>
   );
@@ -27,8 +133,7 @@ const Information = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent:'flex-start',
-    margin:10,
+    margin: 10,
   },
 });
 
