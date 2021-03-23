e933bb65a99d3799ab18ec79e5f6bd8da9083ea3
Biditmangal <biditmangal9506@gmail.com>
2021-03-24 01:41:37 +0530

    Created some dummy screens

---
+++ Minor2_Project/src/components/TabBarIcon.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {Icon} from 'react-native-elements';
+// import Icon from 'react-native-vector-icons/Octicons';
+
+
+import Colors from '../constants/Colors';
+
+export default function TabBarIcon(props) {
+  return (
+    <Icon
+      name={props.name}
+      type={props.type}
+      size={30}
+      // style={{marginBottom: -3}}
+      color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+    />
+  );
+}
