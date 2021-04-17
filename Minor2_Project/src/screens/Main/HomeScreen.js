4bc6a95b6cfa4247a3fc4ab2f4bdd961579ff8c6
Biditmangal <biditmangal9506@gmail.com>
2021-04-18 01:15:19 +0530

    Cleaned Code

--- Minor2_Project/src/screens/Main/HomeScreen.js
+++ Minor2_Project/src/screens/Main/HomeScreen.js
@@ -4,38 +4,46 @@ import {Text, View, ScrollView} from 'react-native';
 import CommunityCard from '../../components/CommunityCard';
 
 const HomeScreen = () => {
-  const [data,setData] = useState({
+  const [data, setData] = useState({
     CommunityData: [
       {
-        userid:'1',
-        thumbnail:'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
-        name:'Joe Stockton',
-        posted:'3 days ago',
-        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate consequat urna, eu faucibus dolor rhoncus a. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+        userid: '1',
+        profilePic:
+          'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
+        name: 'Joe Stockton',
+        posted: '3 days ago',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate consequat urna, eu faucibus dolor rhoncus a. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
       },
       {
-        userid:'2',
-        thumbnail:'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
-        name:'Joe Stockton',
-        posted:'3 days ago',
-        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate consequat urna, eu faucibus dolor rhoncus a. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+        userid: '2',
+        profilePic:
+          'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
+        name: 'Joe Stockton',
+        posted: '3 days ago',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate consequat urna, eu faucibus dolor rhoncus a. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
       },
       {
-        userid:'3',
-        thumbnail:'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
-        name:'Joe Stockton',
-        posted:'3 days ago',
-        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate consequat urna, eu faucibus dolor rhoncus a. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+        userid: '3',
+        profilePic:
+          'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
+        name: 'Joe Stockton',
+        posted: '3 days ago',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate consequat urna, eu faucibus dolor rhoncus a. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
       },
       {
-        userid:'4',
-        thumbnail:'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
-        name:'Joe Stockton',
-        posted:'3 days ago',
-        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate consequat urna, eu faucibus dolor rhoncus a. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-      }
-    ]
-  })
+        userid: '4',
+        profilePic:
+          'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
+        name: 'Joe Stockton',
+        posted: '3 days ago',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate consequat urna, eu faucibus dolor rhoncus a. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+      },
+    ],
+  });
 
   return (
   
