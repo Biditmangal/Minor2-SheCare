9a8ceba1952d8b8461f3c2f1842d4d43b20f3d2b
Biditmangal <biditmangal9506@gmail.com>
2021-04-24 17:37:07 +0530

    Added Redux Store

--- Minor2_Project/index.js
+++ Minor2_Project/index.js
@@ -1,9 +1,19 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
+import React from 'react';
 import App from './App';
 import {name as appName} from './app.json';
+import {PersistGate} from 'redux-persist/integration/react';
+import {Provider} from 'react-redux';
+import {getStore, getPersistor} from './src/redux/store';
 
-AppRegistry.registerComponent(appName, () => App);
+const store = getStore();
+const myPersistor = getPersistor();
+
+const RNRedux = () => (
+  <Provider store={store}>
+    <PersistGate persistor={myPersistor} loading={null}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
+AppRegistry.registerComponent(appName, () => RNRedux);
