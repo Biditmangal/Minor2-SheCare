a1a84f0b1e1977bd832b6c76f0fdd961a5fbf6b2
miglaniparth <parth.miglani2@gmail.com>
2021-06-02 02:10:17 +0530

    Updated dependencies for non UI browser

--- Web_Scraping/scraping Videos/src/main.py
+++ Web_Scraping/scraping Videos/src/main.py
@@ -4,9 +4,31 @@ from webdriver_manager.chrome import ChromeDriverManager
 import random
 import requests
 
+import os
+
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--window-size=1280x1696')
+chrome_options.add_argument('--user-data-dir=/tmp/user-data')
+chrome_options.add_argument('--hide-scrollbars')
+chrome_options.add_argument('--enable-logging')
+chrome_options.add_argument('--log-level=0')
+chrome_options.add_argument('--v=99')
+chrome_options.add_argument('--single-process')
+chrome_options.add_argument('--data-path=/tmp/data-path')
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--homedir=/tmp')
+chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
+chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
+chrome_options.binary_location = os.getcwd() + "/bin/headless-chromium"
+
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument("--headless")
+# driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 links = []  # Initiate empty list to capture final results
 title = []
 desc = []
@@ -29,7 +51,7 @@ def scrape(url):
         desc.append(h.find(class_='IsZvec').text)
     pass
 
-if __name__ == '__main__':
+def lambda_handler(event, context):
     scrape(url1)
     scrape(url2)
     scrape(url3)
